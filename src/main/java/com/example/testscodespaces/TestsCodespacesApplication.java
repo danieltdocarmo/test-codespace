60a9cbeb12cfacb8279b552e52efd1cac780533d
danieltdocarmo <danielt.docarmo@gmail.com>
2025-02-02 11:20:35 -0300

    first commit

---
+++ src/main/java/com/example/testscodespaces/TestsCodespacesApplication.java
@@ -0,0 +1,13 @@
+package com.example.testscodespaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsCodespacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsCodespacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testscodespaces/TestsCodespacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testscodespaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsCodespacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
